b7458c170a33c854729a8576e587469a93326e0b
MiquilW <michael.welsome@gmail.com>
2023-07-24 17:23:02 -0700

    Star fixes

--- index.js
+++ index.js
@@ -2,20 +2,23 @@
 function change() {
   var colors = ['#0FF0FC', '#7FFF00', '#8A2BE2', '#CCFF00', '#FF5E00']
   var stars = document.getElementById("stars").getElementsByTagName('*');
+  var starsList = document.getElementById("stars");
   var e;
+
+  for (var i = 0; i < 60; i++) {
+    var star = document.createElement("h5");
+    star.textContent = "*"
+    starsList.appendChild(star);
+  }
+
   for (var i = 0; i < stars.length; i++) {
     e = stars[i];
-    var randomNumber1 = Math.random() * -800;
-    var randomNumber2 = Math.random() * 400;
-    if (i % 2 == 0) {
-      randomNumber1 = randomNumber1 * -1;
-    }
-    else {
-      randomNumber2 = randomNumber2 * -1;
-    }
+    var randomNumber1 = Math.floor(Math.random() * (800 - -800) ) + -800;
+    var randomNumber2 = Math.floor(Math.random() * (400 - -400) ) + -400;
     var randInt = Math.floor(Math.random() * 5);
     var px = String(randomNumber1) + "px " + String(randomNumber2) + "px"
     e.style.translate = px;
     e.style.color = colors[randInt];
+    e.style.zIndex = "-1";
   }
 }
